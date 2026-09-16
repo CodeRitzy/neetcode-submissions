ed7d1db67f423a0ae35efe002ecfca7fbeedd4ae
CodeRitzy <136943053+CodeRitzy@users.noreply.github.com>
2026-09-16 14:11:44 -0500

    Add: string-encode-and-decode - submission-25

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-25.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        line = ""
+        asciiCount = 129
+        for s in strs:
+            for i in s:
+                line += chr(ord(i)) + chr(asciiCount)
+                asciiCount = asciiCount + 1
+            line += chr(128)
+        return line
+
+    def decode(self, s: str) -> List[str]:
+        word = ""
+        strList = []
+        for i in range(len(s)):
+            if ord(s[i]) == 128:
+                strList.append(word)
+                word = ""
+            if ord(s[i]) >= 129:
+                continue
+            if ord(s[i]) < 128 and s[i] != ord(chr(128)):
+                word += s[i]
+        return strList
